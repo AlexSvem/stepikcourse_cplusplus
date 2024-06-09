118e83831cfd9dfd690876c4617111bae8fa4839
alexsvem <alexeydoborin@gmail.com>
2024-06-09 15:58:06 +0300

    6 commit

--- task1.7/exercise6/main.cpp
+++ task1.7/exercise6/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
 
 int main() {
+    int a, min_size;
+    min_size = INT_MAX;
+    cin >> a;
+    vector<int> s(a);
+    for (int i = 0; i < a; ++i) {
+        cin >> s[i];
+    }
+    for (int i = 0; i < a; ++i) {
+        if (s[i] > 0 && s[i] < min_size){
+            min_size = s[i];
+        }
+    }
+    cout << min_size << endl;
     return 0;
 }
