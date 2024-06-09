cfc905154760b25b4ad0a4a0477d9c51dd6a3227
alexsvem <alexeydoborin@gmail.com>
2024-06-09 14:33:10 +0300

    6 commit

--- task1.7/exercise5/main.cpp
+++ task1.7/exercise5/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    bool found = false;
+    for (int i = 1; i < n &&!found; ++i) {
+        if ((arr[i] > 0 && arr[i - 1] > 0) || (arr[i] < 0 && arr[i - 1] < 0)) {
+            cout << arr[i - 1] << " " << arr[i] << endl;
+            found = true;
+        }
+    }
     return 0;
 }
