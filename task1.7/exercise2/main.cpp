1cb208bf526b43d72ef4f7dc2ec00afefef30822
alexsvem <alexeydoborin@gmail.com>
2024-06-09 13:14:34 +0300

    Second commit

--- task1.7/exercise2/main.cpp
+++ task1.7/exercise2/main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main() {
     int a;
-    cin 
+    cin >> a;
+    vector<int> s(a);
+    for (int i = 0; i < a; i++) {
+        cin >> s[i];
+    }
+    for (int i = 0; i < a; i ++) {
+        if (s[i] % 2 == 0) {
+            cout << s[i];
+        }
+    }
     return 0;
 }
