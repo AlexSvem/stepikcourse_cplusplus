47445e42df7e79224116ca8b2ad4ab3cf3be3a2e
alexsvem <alexeydoborin@gmail.com>
2024-06-09 14:48:01 +0300

    6 commit

--- task1.7/exercise5/main.cpp
+++ task1.7/exercise5/main.cpp
@@ -12,7 +12,11 @@ int main() {
     bool found = false;
     for (int i = 1; i < n &&!found; ++i) {
         if ((arr[i] > 0 && arr[i - 1] > 0) || (arr[i] < 0 && arr[i - 1] < 0)) {
-            cout << arr[i - 1] << " " << arr[i] << endl;
+            if (arr[i] > arr[i-1]) {
+                cout << arr[i-1] << " " << arr[i];
+            } else {
+                cout << arr[i] << " " << arr[i-1];
+            }
             found = true;
         }
     }
