057c5bc7d10dd57dbe980141a9c9efb99249c034
alexsvem <alexeydoborin@gmail.com>
2024-06-09 17:13:57 +0300

    7 commit

--- task1.7/exercise7/main.cpp
+++ task1.7/exercise7/main.cpp
@@ -10,25 +10,21 @@ int main()
     cin >> n;
     vector <int> a;
     vector <int> b;
-    //считывание элементов
     for (int i = 0; i < n; i++) {
         int temp;
         cin >> temp;
         a.push_back(temp);
     }
-    //создание нечетного вектора
     for (int i = 0; i < n; i++) {
         if (a[i] != 0 && a[i] % 2 != 0) {
             vb = a[i];
             b.push_back(vb);
         }
     }
-    //если все четное, то 0
     if (vb == 0) {
         cout << 0;
         return 0;
     }
-    //нахождение минимального из нечетного
 
     min = b[0];
     for (int i = 0; i < b.size(); i++) {
