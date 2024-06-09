b7ab5980721c22435d9703e4a817b889780b4161
alexsvem <alexeydoborin@gmail.com>
2024-06-09 21:04:24 +0300

    8 commit

--- task1.7/exercise8/main.cpp
+++ task1.7/exercise8/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 int main() {
+    int a;
+    cin >> a;
+    vector<int> s(a);
+    for (int i = 0; i < a; ++i) {
+        cin >> s[i];
+    }
+
+    set<int> unique_elements(s.begin(), s.end());
+    cout << unique_elements.size() << endl;
+
     return 0;
 }
