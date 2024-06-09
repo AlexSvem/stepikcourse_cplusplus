e496502bc356a155e79346b76bc865d1f25dfc2a
alexsvem <alexeydoborin@gmail.com>
2024-06-09 13:44:34 +0300

    5 commit

--- task1.7/exercise3/main.cpp
+++ task1.7/exercise3/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-    
+    int a, score = 0;
+    cin >> a;
+    vector<int> s(a);
+    for (int i = 0; i < a; i++) {
+        cin >> s[i];
+    }
+    for (int i = 0; i < a; i++) {
+        if (s[i] > 0) {
+            score++;
+        }
+    }
+    cout << score;
     return 0;
 }
