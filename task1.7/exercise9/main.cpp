8883c14631ce1540050390f3064c8aed9dc6154d
alexsvem <alexeydoborin@gmail.com>
2024-06-14 15:25:12 +0300

    9 commit

--- task1.7/exercise9/main.cpp
+++ task1.7/exercise9/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
 
 int main() {
-
-    return 0;
+    int n, k, tmp;
+    k = 0;
+    cin >> n;
+    vector <int> a(n);
+    //считывание
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    //обработка и вывод
+    for (int i = 1; i < n; i += 2) {
+        cout << a[i] << ' ' << a[i-1] << ' ';
+    }
+    if (n % 2 == 1) {
+        cout << a[n - 1];
+    }
 }
