2178b3a94d57293cde11c86728458f8fc596f67a
alexsvem <alexeydoborin@gmail.com>
2024-06-17 16:58:52 +0300

    11 commit

--- task1.7/exercise11/main.cpp
+++ task1.7/exercise11/main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
+    int q;
+    vector<int> v;
+
+    cin >> q;
+
+    while (cin >> q) {
+        v.push_back(q);
+    }
+
+    int cnt = 0;
+
+    for (int i = 0; i < v.size(); ++i) {
+        for (int j = i + 1; j < v.size(); ++j) {
+            if (v[i] == v[j]) {
+                ++cnt;
+            }
+        }
+    }
+
+    cout << cnt;
+
     return 0;
 }
